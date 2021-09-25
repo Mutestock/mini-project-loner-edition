a0f90e85a42aa20af39e4f6718501ff1aca24491
= <doublenegative@protonmail.com>
2021-09-25 20:30:11 +0200

    bump

--- rest/deps.ts
+++ rest/deps.ts
@@ -7,8 +7,9 @@ import * as oak from "https://deno.land/x/oak/mod.ts";
 import * as asserts from "https://deno.land/std/testing/asserts.ts";
 import * as colors from "https://deno.land/std@0.105.0/fmt/colors.ts";
 import * as classTransformer from 'https://cdn.skypack.dev/class-transformer';
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 //export { Result, Err, Ok } from 'https://cdn.skypack.dev/ts-results';
 
 export {
-    asserts, oak, postgres, toml, colors, classTransformer
+    asserts, oak, postgres, toml, colors, classTransformer, oakCors
 };
