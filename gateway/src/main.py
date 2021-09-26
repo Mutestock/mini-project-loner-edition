d46101731166c788e9ee56b4aa82f34b93d4fc3e
mutestock <doublenegative@protonmail>
2021-09-26 18:45:51 +0200

    gateway gRPC stuff

---
+++ gateway/src/main.py
@@ -0,0 +1,8 @@
+from server.grpc_server import serve
+
+def main() -> None:
+    serve()
+
+
+if __name__ == "__main__":
+    main()
---
+++ gateway/src/utils/run_codegen.py
@@ -0,0 +1,11 @@
+from grpc_tools import protoc
+
+
+
+protoc.main((
+    '',
+    '-I../../proto',
+    '--python_out=.',
+    '--grpc_python_out=.',
+    '../../proto/in/student/student.proto'
+))
