2c7e6763a8c8609790bc7caf6063c93e137598fc
apatheticfrog <apathetic_frog@protonmail.com>
2021-09-26 22:14:11 +0200

    gRPC with timestamps

--- grpc/src/entities/student.rs
+++ grpc/src/entities/student.rs
@@ -1,24 +1,31 @@
 tonic::include_proto!("student");
 
-use serde_derive::{Deserialize, Serialize};
+//use serde_derive::{Deserialize, Serialize};
+use chrono::{NaiveDateTime};
+
 
 // Sqlx => Tonic
-#[derive(Serialize, Deserialize, sqlx::FromRow)]
+#[derive(sqlx::FromRow)]
 pub struct StudentConverter {
     id: i32,
     first_name: String,
     last_name: String,
     phone_number: String,
     email: String,
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime,
 }
 
 impl StudentConverter {
     pub fn to_read_response(&self) -> ReadStudentResponse {
         ReadStudentResponse {
+            id:self.id.clone(),
             first_name: self.first_name.clone(),
             last_name: self.last_name.clone(),
             phone_number: self.phone_number.clone(),
             email: self.email.clone(),
+            updated_at: self.updated_at.to_string(),
+            created_at: self.created_at.to_string(),
         }
     }
     pub fn to_list_response(stud_vec: Vec<StudentConverter>) -> ReadStudentListResponse{
