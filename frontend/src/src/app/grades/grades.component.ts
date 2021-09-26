f96a4aeb0087106ae9c26cbdc3b799b5153e78b5
sofushn <sofushn@hotmail.dk>
2021-09-26 20:15:37 +0200

    Frontend - use scss instead of css

--- frontend/src/src/app/grades/grades.component.ts
+++ frontend/src/src/app/grades/grades.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-grades',
   templateUrl: './grades.component.html',
-  styleUrls: ['./grades.component.css']
+  styleUrls: ['./grades.component.scss']
 })
 export class GradesComponent implements OnInit {
 
