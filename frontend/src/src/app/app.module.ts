76f01ab5f3f0fe5ae6181ed11de83a368a5b5051
sofushn <sofushn@hotmail.dk>
2021-09-26 21:06:28 +0200

    added grpc support to frontend

--- frontend/src/src/app/app.module.ts
+++ frontend/src/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar/';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule }  from '@angular/material/button'
 import { MatTabsModule } from '@angular/material/tabs';
@@ -11,6 +11,8 @@ import { MatTableModule } from '@angular/material/table'
 import { TeachersComponent } from './teachers/teachers.component';
 import { StudentsComponent } from './students/students.component';
 import { GradesComponent } from './grades/grades.component'
+import { GrpcCoreModule } from '@ngx-grpc/core';
+import { GrpcWebClientModule } from '@ngx-grpc/grpc-web-client';
 
 @NgModule({
   declarations: [
@@ -26,7 +28,11 @@ import { GradesComponent } from './grades/grades.component'
     MatIconModule,
     MatButtonModule,
     MatTabsModule,
-    MatTableModule
+    MatTableModule,
+    GrpcCoreModule.forRoot(),
+    GrpcWebClientModule.forRoot({
+      settings: { host: 'http://grpc_dev:10030' }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
