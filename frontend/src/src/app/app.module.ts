003acb886447cc81dead028a07a520392c03352b
sofushn <sofushn@hotmail.dk>
2021-09-26 18:55:06 +0200

    Frontend - Added skeleton for website

--- frontend/src/src/app/app.module.ts
+++ frontend/src/src/app/app.module.ts
@@ -3,16 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar/';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule }  from '@angular/material/button'
+import { MatTabsModule } from '@angular/material/tabs';
+import { TeachersComponent } from './teachers/teachers.component';
+import { StudentsComponent } from './students/students.component';
+import { GradesComponent } from './grades/grades.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeachersComponent,
+    StudentsComponent,
+    GradesComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {  }
