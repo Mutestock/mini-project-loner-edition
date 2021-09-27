7f48744972618a386eea83d31ecaaef37e00c155
apatheticfrog <apathetic_frog@protonmail.com>
2021-09-27 21:35:54 +0200

    flesh out gRPC gateway

--- gateway/src/server/servicers/student_servicer.py
+++ gateway/src/server/servicers/student_servicer.py
@@ -1,4 +1,25 @@
 from logic.protogen.out import student_out_pb2_grpc
+from logic.protogen.out import student_out_pb2
+
 
 class StudentServicer(student_out_pb2_grpc.StudentOutServicer):
-    pass
+
+    def CreateStudent(self, request, context):
+        return student_out_pb2.CreateStudentResponse("Placeholder")
+    
+    def ReadStudent(self, request, context):
+        return student_out_pb2.ReadStudentResponse("Placeholder")
+    
+    def UpdateStudent(self, request, context):
+        return student_out_pb2.UpdateStudentResponse("Placeholder")
+    
+    def DeleteStudent(self, request, context):
+        return student_out_pb2.DeleteStudentResponse("Placeholder")
+    
+    def ReadStudentList(self, request, context):
+        return student_out_pb2.ReadStudentListResponse("Placeholder")
+    
+    
+    
+    
+
