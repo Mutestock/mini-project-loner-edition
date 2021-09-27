488cfca60c693b49e443c1b08847efc993ec291d
sofushn <sofushn@hotmail.dk>
2021-09-27 19:22:30 +0200

    Added rest service for integrating with soap service

---
+++ teacher_rest/src/Controllers/TeachersController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SoapServiceReference;
+
+namespace TeacherRestService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TeachersController : ControllerBase
+    {
+        private readonly ILogger<TeachersController> _logger;
+        private readonly ITeacherService _teacherService;
+
+        public TeachersController(ILogger<TeachersController> logger, ITeacherService teacherService)
+        {
+            _logger = logger;
+            _teacherService = teacherService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Teacher>> GetTeachers()
+           => await _teacherService.GetAllTeachersAsync();
+    }
+}
